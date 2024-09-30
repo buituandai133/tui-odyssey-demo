7e43923f68e514a9fd1f954ce0aad2b02eff5435
Dai Bui <buituandai133@gmail.com>
2024-09-30 13:50:40 +0700

    Add key

--- src/app/tours/page.tsx
+++ src/app/tours/page.tsx
@@ -158,7 +158,7 @@ export default function Tours() {
             <h2 className="text-3xl font-bold mb-8 text-gray-800 dark:text-gray-200">Flash Sales</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
               {flashSales.map((tour, index) => (
-                <TourCard {...tour} />
+                <TourCard key={index} {...tour} />
               ))}
             </div>
           </div>
